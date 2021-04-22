33efec5c33d7cafa8a0911eb3471cb14fbb5ee76
hellolimi <hellolaimmn@gmail.com>
2021-04-22 20:27:26 +0100

    add form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-    return <span>
-        Home
-    </span>;
+    const [lmitte, setLmitte] = useState('');
+
+    const onSubmit= e => {
+        e.preventDefault();
+    }
+    const onChange = e => {
+        const {value} = e.target;
+        setLmitte(value);
+    }
+    return(
+        <>
+            <form onSubmit={onSubmit}>
+                <input value={lmitte} type="text" placeholder="What's on your mind?" onChange={onChange} />
+                <input type="submit" value="Lmitte" />
+            </form>
+        </>
+    );
+    
 }
 
 export default Home;
