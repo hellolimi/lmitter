ce30b18896133e8c01dc12a3c142cb4625efa789
hellolimi <hellolaimmn@gmail.com>
2021-04-23 22:35:56 +0100

    remove unused imported files

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Auth from 'routes/Auth';
 import Home from 'routes/Home';
