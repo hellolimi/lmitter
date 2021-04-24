834f144ec26ca2e257b1e45279b92eae37970557
hellolimi <hellolaimmn@gmail.com>
2021-04-24 14:44:36 +0100

    delete redirect / controlling from profile.js & tweak profile link

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Auth from 'routes/Auth';
 import Home from 'routes/Home';
 import Navigation from 'components/Navigation';
 import Profile from 'routes/Profile';
 
-const AppRouter = ({isLoggedIn, userObj}) => {
+const AppRouter = ({isLoggedIn, userObj, refreshUser}) => {
     return (
         <Router>
             <Switch>
                 {isLoggedIn?
                     <>
-                        <Navigation />
+                        <Navigation userObj={userObj} />
                         <Route exact path="/">
                             <Home userObj={userObj} />
                         </Route>
                         <Route exact path="/profile">
-                            <Profile />
+                            <Profile userObj={userObj} refreshUser={refreshUser} />
                         </Route>
-                        <Redirect from="*" to="/" />
                     </>
                     :
                     <>
                         <Route exact path="/">
                         <Auth />
                         </Route>
-                        <Redirect from="*" to="/" />
                     </>
                 }
             </Switch>
