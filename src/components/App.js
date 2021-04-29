2eca90fdcaa153000160117fb9e31798b138c155
hellolimi <hellolaimmn@gmail.com>
2021-04-29 18:05:45 +0100

    set profile photo when accounts are created / function moved to AccountForm.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authSerive } from 'myBase';
-import logo from 'img/logo512.png';
 
 function App() {
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
+
   useEffect(() => {
     authSerive.onAuthStateChanged((user) => {
       if(user){
-        let proPhoto;
-        if(user.photoURL){
-          proPhoto = user.photoURL;
-        }else{
-          proPhoto = logo;
-        }
         setUserObj({
           displayName : user.displayName,
           uid: user.uid,
-          photoURL: proPhoto,
+          photoURL: user.photoURL,
           updateProfile: (args) => user.updateProfile(args),
         });
       }else{
@@ -30,10 +24,9 @@ function App() {
       setUserObj(null);
     }
   }, []);
+
   const refreshUser = () => {
-    
     const user = authSerive.currentUser;
-    
     setUserObj({
       displayName : user.displayName,
       uid: user.uid,
@@ -41,6 +34,7 @@ function App() {
       updateProfile: (args) => user.updateProfile(args)
     });
   }
+
   return <>
     {init?<AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} refreshUser={refreshUser} />:"Initializing"}
     <footer>&copy; {new Date().getFullYear()} Lmitter</footer>
