2a8be7c411cac284719125dbf9a4c11c6cfdda97
hellolimi <hellolaimmn@gmail.com>
2021-05-02 16:59:26 +0100

    add like/comment api

--- src/components/Lmitte.js
+++ src/components/Lmitte.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import { dbService, storageService } from 'myBase';
 import { Link } from 'react-router-dom';
+import { MdFavoriteBorder, MdFavorite, MdChatBubbleOutline } from "react-icons/md";
 
-const Lmitte = ({lmitteObj, isOwner}) => {
+const Lmitte = ({lmitteObj, userUid}) => {
     const [edit, setEdit] = useState(false);
     const [newLmitte, setNewLmitte] = useState(lmitteObj.text);
+
+    const [like, setLike] = useState(false);
+    const [comment, setComment] = useState(false);
+    const [input, setInput] = useState();
+    const [comId, setComId] = useState(0);
 
     let TIME = Date.now();
     let timeView;
@@ -45,11 +51,43 @@ const Lmitte = ({lmitteObj, isOwner}) => {
            await dbService.doc(`lmittes/${lmitteObj.id}`).delete();
         }
     }
-
+    const { likedId, comments } = lmitteObj;
+    const onLike = async (e) => {
+        e.preventDefault();
+        setLike(prev => !prev);
+        if(like){
+            await dbService.doc(`lmittes/${lmitteObj.id}`).update({
+                likedId : likedId.concat(userUid)
+            })
+        }else{
+            await dbService.doc(`lmittes/${lmitteObj.id}`).update({
+                likedId : likedId.filter(id => id !== userUid)
+            })
+        }
+    }
+    const commentToggle = () => {
+        setComment(prev => !prev);
+    }
+    const onChangeComment = e => {
+        const {value} = e.target;
+        setInput(value);
+    }
+    const onSubmitComment = async (e) => {
+        e.preventDefault();
+        const aComment = {
+            text: input,
+            creatorId : userUid,
+            id : comId
+        }
+        await dbService.doc(`lmittes/${lmitteObj.id}`).update({
+            comments : comments.concat(aComment)
+        })
+        setComId(prev => prev++);
+    }
     return(
         <li>
             {edit?<>
-                    {isOwner&&<>
+                    {userUid === lmitteObj.creatorId&&<>
                         <form onSubmit={onSubmit}>
                             <input type="text" placeholder="Edit your lmitte!" value ={newLmitte} required onChange={onChange} />
                             <button type="submit">Update Lmitte</button>
@@ -67,9 +105,20 @@ const Lmitte = ({lmitteObj, isOwner}) => {
                     <h4>{lmitteObj.text}</h4>
                     {lmitteObj.fileUrl && <img src={lmitteObj.fileUrl} width="100px" alt="" />}
                     <span className="date">{timeView}</span>
-                    {isOwner&&<>
+                    {userUid === lmitteObj.creatorId&&<>
                             <button onClick={toggleEdit}>edit</button>
                             <button onClick={onDeleteClick}>delete</button>
+                    </>}
+                    <button onClick={onLike}>{like?<MdFavorite />:<MdFavoriteBorder />}</button>
+                    <span>{comments.length}</span><button onClick={commentToggle}><MdChatBubbleOutline /></button>
+                    {comment&&<>
+                        <form onSubmit={onSubmitComment}>
+                            <input type="text" placeholder="Leave your comment" onChange={onChangeComment} />
+                            <button>comment</button>
+                        </form>
+                        {comments.length>0&&<ul className="comment">
+                            {comments.map(comment => <li key={comId}>{comment.text}</li>)}
+                        </ul>}
                     </>}
             </>}
         </li>
